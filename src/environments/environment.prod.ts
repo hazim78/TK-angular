65541a4708fbec0405ffaaadd84e9f1cdcc020f4
Wan Akashah <wanakashah14833@gmail.com>
2018-03-27 18:43:55 +0800

    Initialized Angular with Bootstrap and Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAjGyF5CPZHWmCimWPc9gzF0sCCWbNHk0k",
+    authDomain: "tk-angular.firebaseapp.com",
+    databaseURL: "https://tk-angular.firebaseio.com",
+    projectId: "tk-angular",
+    storageBucket: "tk-angular.appspot.com",
+    messagingSenderId: "41345624654"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAjGyF5CPZHWmCimWPc9gzF0sCCWbNHk0k",
+    authDomain: "tk-angular.firebaseapp.com",
+    databaseURL: "https://tk-angular.firebaseio.com",
+    projectId: "tk-angular",
+    storageBucket: "tk-angular.appspot.com",
+    messagingSenderId: "41345624654"
+  }
 };
